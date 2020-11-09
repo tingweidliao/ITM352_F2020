a7daf0881948d874f7fdcb0f0ce9c51be289b3f3
David <59951624+tingweidliao@users.noreply.github.com>
2020-11-08 23:40:03 -1000

    update

---
+++ Liao_David_Assignment1/public/anti_meta_cards/product_data.js
@@ -0,0 +1,58 @@
+//David Liao
+
+// Master array with my products 
+// products found on ygoprodeck.com
+
+var products = [
+
+{
+
+    "Product": "Duelist Evolution - Dark Magician",
+    "price": 102.55,
+    "image": "https://ygoprodeck.com/wp-content/uploads/2020/11/DuelitEvolution-Deck1-deck-122123.jpg",
+
+},
+
+{
+
+    "Product": "Cyber Dragon 2020",
+    "price": 307.06,
+    "image": "https://ygoprodeck.com/wp-content/uploads/2020/04/CyberDragonDeck2020-deck-80157.png",
+
+},
+
+{
+
+    "Product": "Blue-Eyes 2020",
+    "price": 132.60,
+    "image": "https://ygoprodeck.com/wp-content/uploads/2020/01/Blue-deck-69017.png",
+
+},
+
+{
+
+    "Product": "Exodia 2020",
+    "price": 132.60,
+    "image": "https://ygoprodeck.com/wp-content/uploads/2020/02/ExodiaDeck2020-deck-75593.png",
+
+},
+
+{
+
+    "Product": "Subterror Princess",
+    "price": 250.29,
+    "image": "https://ygoprodeck.com/wp-content/uploads/2020/11/SubterrorPrince-deck-121727.jpg",
+
+},
+];
+
+// template borrowed from assignment 1 example
+
+
+if (typeof module != 'undefined') {
+    module.exports.products = products;
+    }
+
+
+
+
