4743728203e9700be1380ecc1f107354e2b12a71
David <59951624+tingweidliao@users.noreply.github.com>
2020-12-18 22:43:05 -1000

    update

--- Liao_David_Assignment3/server.js
+++ Liao_David_Assignment3/server.js
@@ -1,133 +1,164 @@
-/*
-David Liao
-6 Dec 2020
-Assignment 3 template
-Example code copied from https://codepen.io/khurramalvi/pen/EKRQJZ
-Oringinal author Khurram Alvi 
+// David Liao
+// Server page for A3
+// 16 Dec 2020
+// code from assignments 1&2 & file IO
 
+var data = require('./public/products.js'); //load products.js file and set to variable 'data'
+var products_array = data.products; //set variable 'products_array' to the products array in the products.js file
+const queryString = require('query-string'); //read variable 'queryString' as the loaded query-string module
+var express = require('express'); //load and cache express module
+var app = express(); //set module to variable 'app'
+var myParser = require("body-parser"); //load and cache body parser module
+var filename = 'user_data.json'; // sets the variable filename to user_data.json
+var fs = require('fs'); //Load file system
 
-/* Set values + misc */
-var promoCode;
-var promoPrice;
-var fadeTime = 300;
-
-/* Assign actions */
-$('.quantity input').change(function() {
-  updateQuantity(this);
+app.all('*', function (request, response, next) { //for all request methods...
+    console.log(request.method + ' to ' + request.path); //write in the console the request method and its path
+    next(); //move on
 });
 
-$('.remove button').click(function() {
-  removeItem(this);
+app.use(myParser.urlencoded({ extended: true })); //get data in the body
+
+// login stuff 
+if (fs.existsSync(filename)) { //enuring that the variable filename exists
+  stats = fs.statSync(filename) //gets the information from user_data.json
+  console.log(filename + 'has' + stats.size + 'characters'); //recording the amount of characters in the console 
+
+  data = fs.readFileSync(filename, 'utf-8');
+  users_reg_data = JSON.parse(data);
+} else { 
+  console.log(filename + 'does not exist!'); //if filename doesn't exist showing it in the server
+}
+
+//processing a users's login through a post on the server 
+app.post("/process_login", function (req, res) {
+    var LogError = [];
+    console.log(req.query);
+    the_username = req.body.username.toLowerCase(); //putting the users, username as all lowercase
+    if (typeof users_reg_data[the_username] != 'undefined') { //ask the object if it has matching username or leaving it as undefined
+        if (users_reg_data[the_username].password == req.body.password) {
+            req.query.username = the_username; 
+            console.log(users_reg_data[req.query.username].name);
+            req.query.name = users_reg_data[req.query.username].name
+            res.redirect('/invoice.html?' + queryString.stringify(req.query));
+            return; //this will redirect them to the invoice if username and password are entered correctly 
+        } else { //if password is not entered correctly tells the user invalid password 
+            LogError.push = ('Invalid Password');
+            console.log(LogError);
+            req.query.username= the_username;
+            req.query.name= users_reg_data[the_username].name;
+            req.query.LogError=LogError.join(';');
+        }
+        } else { //if username is incorrect push to the user invalid username 
+            LogError.push = ('Invalid Username');
+            console.log(LogError);
+            req.query.username= the_username;
+            req.query.LogError=LogError.join(';');
+        }
+    res.redirect('./login.html?' + queryString.stringify(req.query));
 });
 
-$(document).ready(function() {
-  updateSumItems();
-});
+//creates an account on the server side 
+app.post("/process_register", function (req, res) {
+    qstr = req.body
+    console.log(qstr);
+    var errors = [];
 
-$('.promo-code-cta').click(function() {
-
-  promoCode = $('#promo-code').val();
-
-  if (promoCode == '10off' || promoCode == '10OFF') {
-    //If promoPrice has no value, set it as 10 for the 10OFF promocode
-    if (!promoPrice) {
-      promoPrice = 10;
-    } else if (promoCode) {
-      promoPrice = promoPrice * 1;
+    if (/^[A-Za-z]+$/.test(req.body.name)) { //forces the use of only letters for Full Naame
     }
-  } else if (promoCode != '') {
-    alert("Invalid Promo Code");
-    promoPrice = 0;
+    else {
+      errors.push('Use Only Letters for Full Name')
+    }
+    // validating that it is a Full Name
+    if (req.body.name == "") {
+      errors.push('Invalid Full Name');
+    }
+    // length of full name is between 0 and 25 
+  if ((req.body.fullname.length > 25 && req.body.fullname.length <0)) {
+    errors.push('Full Name Too Long')
   }
-  //If there is a promoPrice that has been set (it means there is a valid promoCode input) show promo
-  if (promoPrice) {
-    $('.summary-promo').removeClass('hide');
-    $('.promo-value').text(promoPrice.toFixed(2));
-    recalculateCart(true);
-  }
+  //checks the new username in all lowercase against the record of usernames
+    var reguser = req.body.username.toLowerCase(); 
+    if (typeof users_reg_data[reguser] != 'undefined') { //if username is not undefined gives an error that the username is taken
+      errors.push('Username taken')
+    }
+    //requires that the username only be letters and numbers 
+    if (/^[0-9a-zA-Z]+$/.test(req.body.username)) {
+    }
+    else {
+      errors.push('Letters And Numbers Only for Username')
+    }
+  
+    //password is min 6 characters long 
+    if (req.body.password.length < 6) {
+      errors.push('Password Too Short')
+    }
+    // check to see if the two passwords match
+    if (req.body.password !== req.body.repeat_password) { 
+      errors.push('Password Not a Match')
+    }
+    //if there are no errors this saves the user's registration in the json made with help from lab 14
+    if (errors.length == 0) {
+      POST = req.body
+      console.log('no errors')
+      var username = POST['username']
+      users_reg_data[username] = {}; //make it 'users'
+      users_reg_data[username].name = username;
+      users_reg_data[username].password= POST['password'];
+      users_reg_data[username].email = POST['email'];
+      data = JSON.stringify(users_reg_data); //change to users 
+      fs.writeFileSync(filename, data, "utf-8");
+      res.redirect('./invoice.html?' + queryString.stringify(req.query));
+    }
+    //of there are errors log them in the console and direct user again to the register page
+    if (errors.length > 0) {
+        console.log(errors)
+        req.query.name = req.body.name;
+        req.query.username = req.body.username;
+        req.query.password = req.body.password;
+        req.query.repeat_password = req.body.repeat_password;
+        req.query.email = req.body.email;
+
+        req.query.errors = errors.join(';');
+        res.redirect('register.html?' + queryString.stringify(req.query));
+    }
 });
 
-/* Recalculate cart */
-function recalculateCart(onlyTotal) {
-  var subtotal = 0;
+//This part is for processing the purchase and rendering the invoice on the server 
+app.post("/process_purchase", function (request, response) {
+    let POST = request.body; // data would be packaged in the body
 
-  /* Sum up row totals */
-  $('.basket-product').each(function() {
-    subtotal += parseFloat($(this).children('.subtotal').text());
-  });
-
-  /* Calculate totals */
-  var total = subtotal;
-
-  //If there is a valid promoCode, and subtotal < 10 subtract from total
-  var promoPrice = parseFloat($('.promo-value').text());
-  if (promoPrice) {
-    if (subtotal >= 10) {
-      total -= promoPrice;
-    } else {
-      alert('Order must be more than £10 for Promo code to apply.');
-      $('.summary-promo').addClass('hide');
+    //check if quantities are nonnegative integers 
+    if (typeof POST['submitPurchase'] != 'undefined') {
+        var hasvalidquantities=true; // creating a varibale assuming that it'll be true
+        var hasquantities=false
+        for (i = 0; i < products.length; i++) {
+            
+                        qty=POST[`quantity${i}`];
+                        hasquantities=hasquantities || qty>0; // If it has a value bigger than 0 then it is good
+                        hasvalidquantities=hasvalidquantities && isNonNegInt(qty);    // if it is both a quantity over 0 and is valid    
+        } 
+        // if all quantities are valid, generate the invoice// 
+        const stringified = queryString.stringify(POST);
+        if (hasvalidquantities && hasquantities) {
+          response.redirect("./login.html?"+stringified);
+          return; //stops the function 
+        }  
+        else { 
+            response.redirect("./products_display.html?" + stringified) 
+        }
     }
-  }
+});
 
-  /*If switch for update only total, update only total display*/
-  if (onlyTotal) {
-    /* Update total display */
-    $('.total-value').fadeOut(fadeTime, function() {
-      $('#basket-total').html(total.toFixed(2));
-      $('.total-value').fadeIn(fadeTime);
-    });
-  } else {
-    /* Update summary display. */
-    $('.final-value').fadeOut(fadeTime, function() {
-      $('#basket-subtotal').html(subtotal.toFixed(2));
-      $('#basket-total').html(total.toFixed(2));
-      if (total == 0) {
-        $('.checkout-cta').fadeOut(fadeTime);
-      } else {
-        $('.checkout-cta').fadeIn(fadeTime);
-      }
-      $('.final-value').fadeIn(fadeTime);
-    });
-  }
+//repeats the isNonNegInt function from the products_display.html file 
+function isNonNegInt(q, returnErrors = false) {
+    errors = []; // assume that quantity data is valid 
+    if (q == "") { q = 0; }
+    if (Number(q) != q) errors.push('Not a number!'); //check if the string is a number
+    if (q < 0) errors.push('Negative value!'); //check if value is a positive
+    if (parseInt(q) != q) errors.push('Not an integer!'); //check if value is an integer
+    return returnErrors ? errors : (errors.length == 0);
 }
 
-/* Update quantity */
-function updateQuantity(quantityInput) {
-  /* Calculate line price */
-  var productRow = $(quantityInput).parent().parent();
-  var price = parseFloat(productRow.children('.price').text());
-  var quantity = $(quantityInput).val();
-  var linePrice = price * quantity;
-
-  /* Update line price display and recalc cart totals */
-  productRow.children('.subtotal').each(function() {
-    $(this).fadeOut(fadeTime, function() {
-      $(this).text(linePrice.toFixed(2));
-      recalculateCart();
-      $(this).fadeIn(fadeTime);
-    });
-  });
-
-  productRow.find('.item-quantity').text(quantity);
-  updateSumItems();
-}
-
-function updateSumItems() {
-  var sumItems = 0;
-  $('.quantity input').each(function() {
-    sumItems += parseInt($(this).val());
-  });
-  $('.total-items').text(sumItems);
-}
-
-/* Remove item from cart */
-function removeItem(removeButton) {
-  /* Remove row from DOM and recalc cart total */
-  var productRow = $(removeButton).parent().parent();
-  productRow.slideUp(fadeTime, function() {
-    productRow.remove();
-    recalculateCart();
-    updateSumItems();
-  });
-}
+app.use(express.static('./public')); // root in the 'public' directory so that express will serve up files from here
+app.listen(8080, () => console.log(`listening on port 8080`)); //run the server on port 8080 and show it in the consolenpm
